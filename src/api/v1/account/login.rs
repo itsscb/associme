168c3d0a57edfdd25313ffa56906ef74b23b8708
itsscb <dev@itsscb.de>
2025-01-11 23:20:14 +0100

    add token expirations and role to login response

--- src/api/v1/account/login.rs
+++ src/api/v1/account/login.rs
@@ -44,38 +44,39 @@ pub async fn login(
 
     info!(email = &auth.email, ip = ip, user_agent = user_agent);
     let expiration = Utc::now() + Duration::from_secs(60 * 15);
-    let mut claims: paseto_maker::Claims =
-        paseto_maker::Claims::new().with_expiration(expiration.to_rfc3339());
+    let mut claims: paseto_maker::Claims = paseto_maker::Claims::new()
+        .with_expiration(expiration.to_rfc3339())
+        .with_subject("token");
     if let Err(err) = claims.set_claim("role", &role) {
         error!(error = ?err, ip = ip, user_agent = user_agent);
         return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
     }
-    // let mut claims = ClaimsS::new();
-    // if let Err(err) = claims.add("role", &role) {
-    //     return err.into_response();
-    // }
 
-    // let maker = paseto_maker::Maker::new(&config.keypair.secret).unwrap();
-    // let (token, refresh_token) = match TokenS::new_pair(
-    //     id.to_string(),
-    //     claims,
-    //     Duration::from_secs(60 * 15),
-    //     Duration::from_secs(60 * 60 * 24),
-    //     &config.keypair.secret,
-    // ) {
-    //     Ok((token, refresh_token)) => (token, refresh_token),
-    //     Err(e) => {
-    //         error!(error = ?e, ip = ip, user_agent = user_agent);
-    //         return (
-    //             StatusCode::INTERNAL_SERVER_ERROR,
-    //             axum::response::Json(json!({ "error": "Internal server error" })),
-    //         )
-    //             .into_response();
-    //     }
-    // };
+    let token = match config.token_maker.create_token(&claims) {
+        Ok(token) => token,
+        Err(err) => {
+            error!(error = ?err, ip = ip, user_agent = user_agent);
+            return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
+        }
+    };
 
-    let token = &config.token_maker.create_token(&claims).unwrap();
-    let refresh_token = &config.token_maker.create_token(&claims).unwrap();
+    let ref_expiration = Utc::now() + Duration::from_secs(60 * 60 * 24);
+    let mut ref_claims: paseto_maker::Claims = paseto_maker::Claims::new()
+        .with_expiration(ref_expiration.to_rfc3339())
+        .with_subject("refresh_token");
+
+    if let Err(err) = ref_claims.set_claim("role", &role) {
+        error!(error = ?err, ip = ip, user_agent = user_agent);
+        return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
+    }
+
+    let refresh_token = match config.token_maker.create_token(&claims) {
+        Ok(token) => token,
+        Err(err) => {
+            error!(error = ?err, ip = ip, user_agent = user_agent);
+            return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
+        }
+    };
     info!(
         // expires_at = token.expires_at().to_rfc3339(),
         // account_id = token.id(),
@@ -89,7 +90,7 @@ pub async fn login(
         &id,
         user_agent,
         ip,
-        refresh_token,
+        &refresh_token,
         &expiration,
         // refresh_token.expires_at(),
     )
@@ -100,7 +101,7 @@ pub async fn login(
     };
     (
         StatusCode::OK,
-        axum::response::Json(json!({"token": token, "refresh_token": refresh_token})),
+        axum::response::Json(json!({"token": json!({"token": token, "role": role, "expires_at": expiration}), "refresh_token": json!({"token": refresh_token,"expires_at": ref_expiration})})),
     )
         .into_response()
 }
